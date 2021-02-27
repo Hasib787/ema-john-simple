2db7bd950c8fc59c814252be315d012bd9dd961d
Hasib787 <mr.hasib45@gmail.com>
2021-02-27 23:55:52 +0600

    added a image

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,8 +3,13 @@ import React from 'react';
 const Product = (props) => {
     console.log(props.product.name);
     return (
-        <div>
-            <h4>{props.product.name}</h4>
+        <div className="product">
+           <div>
+                <img src={props.product.img} alt=""/>
+           </div>
+           <div>
+                <h4>{props.product.name}</h4>
+           </div>
         </div>
     );
 };
