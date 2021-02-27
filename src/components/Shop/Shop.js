58f0082618fb21568b5372445bb870cd308326f0
Hasib787 <mr.hasib45@gmail.com>
2021-02-27 19:39:33 +0600

    added some changes

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import './Shop.css';
 
 const Shop = () => {
     const fast10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(fast10);
     return (
-        <div>
-            <h1>This is Shop</h1>
-            <h3>{products.length}</h3>
-            <ul>
-                {
-                    products.map(product=> <li>{product.name}</li>)
-                }
-            </ul>
+        <div className="shop-container">
+            <div className="product-container">
+                <ul>
+                    {
+                        products.map(product=> <li>{product.name}</li>)
+                    }
+                </ul>
+            </div>
+            <div className="cart-container">
+                    <h3>This is a cart</h3>
+            </div>
         </div>
     );
 };
