cc7547cad381ca6a3174e36a3850447fca6f3c1b
Hasib787 <mr.hasib45@gmail.com>
2021-03-04 20:11:52 +0600

    added a component and product route

---
+++ src/components/productDetails/ProductDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetails = () => {
+    return (
+        <div>
+            <h1>Product Details is coming sooooon...</h1> 
+        </div>
+    );
+};
+
+export default ProductDetails;
