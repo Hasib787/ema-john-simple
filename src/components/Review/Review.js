5c83b8aeb3f75d0b5d6a949a4145bb9892b7855b
Hasib787 <mr.hasib45@gmail.com>
2021-03-05 23:28:53 +0600

    added some change

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getDatabaseCart } from '../../utilities/databaseManager';
 
 const Review = () => {
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        //cart
+        const savedData = getDatabaseCart();
+        console.log(savedData);
+    }, [])
     return (
         <div>
             <h1>This is review</h1>
