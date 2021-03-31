e4a5271e8f948e601a63628a87f05e1d00117162
Hasib787 <mr.hasib45@gmail.com>
2021-04-01 03:05:18 +0600

    insert bulk data and many data to the databse

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import fakeData from '../../fakeData';
 
 const Inventory = () => {
+        const handleAddProduct=() =>{
+            fetch('http://localhost:5000/addProduct',{
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(fakeData)
+            })
+            
+        }
     return (
         <div>
-            <h2>Inventory Details</h2>
+            <button onClick={handleAddProduct}>Add Product</button>
         </div>
     );
 };
