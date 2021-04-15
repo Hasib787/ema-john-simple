389e66f407ee2bb64a99348279e1ab991c4bea19
Hasib787 <mr.hasib45@gmail.com>
2021-04-15 16:22:24 +0600

    added stripe payment getway

--- src/components/processPayment/ProcessPayment.js
+++ src/components/processPayment/ProcessPayment.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Elements, CardElement } from '@stripe/react-stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import SimpleCardForm from './SimpleCardForm';
 
 const stripePromise = loadStripe('pk_test_51IfDb4BioGMnkIPG0h9x4j8eeFW1xQoO3i0TXhQoEBMcmgp8vcwhH1jkQAqhgysYRZccp6JTcWypP3WHXYUcdjox00vTTu5vRk');
 
@@ -9,22 +10,7 @@ const ProcessPayment = () => {
     return (
         <div>
             <Elements stripe={stripePromise}>
-                <CardElement
-                    options={{
-                        style: {
-                            base: {
-                                fontSize: '16px',
-                                color: '#424770',
-                                '::placeholder': {
-                                    color: '#aab7c4',
-                                },
-                            },
-                            invalid: {
-                                color: '#9e2146',
-                            },
-                        },
-                    }}
-                />
+                <SimpleCardForm></SimpleCardForm>
             </Elements>
         </div>
     );
